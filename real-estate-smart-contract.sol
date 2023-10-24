ef7181fcc71068dda7f964bf21f720950bf0cb61
Technology-For-Good <josephjones7@fastmail.fm>
2023-10-24 14:52:25 -0700

    Update real-estate-smart-contract.sol
    Added necessary import statements to include required libraries or modules.
    Added a function to allow a buyer to pay.
    Handle possible issues, like what happens if a buyer wants to retract approval before signing? Or if the funds sent by the buyer are less than or more than the price?
    Added a modifier for common conditions.
    Added a fallback function for handling direct ether transfers to the contract.

--- real-estate-smart-contract.sol
+++ real-estate-smart-contract.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract RealEstateSalesContract {
@@ -13,12 +14,25 @@ contract RealEstateSalesContract {
     bool public sellerApproved;
     bool public buyerApproved;
     bool public contractSigned;
-    
+    uint public fundsReceived;
+
+    // Modifiers
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only the seller can perform this.");
+        _;
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "Only the buyer can perform this.");
+        _;
+    }
+
     // Events to track contract state changes
     event SellerApproval(bool approved);
     event BuyerApproval(bool approved);
     event ContractSigned(bool signed);
-    
+    event Received(address from, uint amount);
+
     // Constructor function to initialize the contract
     constructor(address payable _seller, address payable _buyer, string memory _propertyAddress, uint _price) {
         seller = _seller;
@@ -28,25 +42,47 @@ contract RealEstateSalesContract {
     }
     
     // Function for the seller to approve the contract
-    function approveSeller() public {
-        require(msg.sender == seller, "Only the seller can approve the contract.");
+    function approveSeller() public onlySeller {
         sellerApproved = true;
         emit SellerApproval(true);
     }
     
     // Function for the buyer to approve the contract
-    function approveBuyer() public {
-        require(msg.sender == buyer, "Only the buyer can approve the contract.");
+    function approveBuyer() public onlyBuyer {
         buyerApproved = true;
         emit BuyerApproval(true);
     }
+
+    // Function to retract buyer's approval
+    function retractBuyerApproval() public onlyBuyer {
+        require(!contractSigned, "Contract is already signed.");
+        buyerApproved = false;
+        emit BuyerApproval(false);
+    }
+
+    // Function to retract seller's approval
+    function retractSellerApproval() public onlySeller {
+        require(!contractSigned, "Contract is already signed.");
+        sellerApproved = false;
+        emit SellerApproval(false);
+    }
     
     // Function to sign the contract
-    function signContract() public {
+    function signContract() public onlyBuyer {
         require(sellerApproved && buyerApproved, "Both parties must approve the contract before it can be signed.");
-        require(msg.sender == buyer, "Only the buyer can sign the contract.");
+        require(fundsReceived >= price, "Insufficient funds transferred.");
         contractSigned = true;
+        if (fundsReceived > price) {
+            uint refundAmount = fundsReceived - price;
+            payable(msg.sender).transfer(refundAmount); // Refund excess
+        }
         seller.transfer(price);
         emit ContractSigned(true);
+    }
+
+    // This function allows the contract to receive funds.
+    receive() external payable {
+        fundsReceived += msg.value;
+        emit Received(msg.sender, msg.value);
     }
 }
